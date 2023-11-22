d1acdf95f8978d92486fcac487db7cd41ba507e0
nanthedom <naufnan@gmail.com>
2023-11-22 20:26:39 +0700

    fix: ADT tree

--- ADT/tree/tree.c
+++ ADT/tree/tree.c
@@ -2,222 +2,202 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-BinTree NewTree (ElType root, BinTree left_tree, BinTree right_tree)
-/* Menghasilkan sebuah pohon biner dari root, left_tree, dan right_tree, jika alokasi berhasil 
-   Menghasilkan pohon kosong (NULL) jika alokasi gagal */
-{
-    /* KAMUS */
-    BinTree p;
-
-    /* ALGORITMA */
-    p = (Address) malloc(sizeof(TreeNode));
-    if (p != NULL) {
-        ROOT(p) = root;
-        LEFT(p) = left_tree;
-        RIGHT(p) = right_tree;
-    }
-    return p;
-}
-
-void CreateTree (ElType root, BinTree left_tree, BinTree right_tree, BinTree *p)
-/* I.S. Sembarang
-   F.S. Menghasilkan sebuah pohon p
-   Menghasilkan sebuah pohon biner p dari akar, l, dan r, jika alokasi 
-   berhasil 
-   Menghasilkan pohon p yang kosong (NULL) jika alokasi gagal */
-{
-    /* KAMUS */
-
-    /* ALGORITMA */
-    *p = NewTree(root, left_tree, right_tree);
-}
-
-Address newTreeNode(ElType val)
-/* Alokasi sebuah address p, bernilai tidak NULL jika berhasil */
-/* Mengirimkan address hasil alokasi sebuah elemen bernilai val
-   Jika alokasi berhasil, maka address tidak NULL, dan misalnya 
-   menghasilkan p, maka p↑.info=val, p↑.left=NULL, p↑.right=NULL 
+TreeNode *createNode(ElType val)
+/* Fungsi untuk membuat node baru dengan data tertentu */
+/* Alokasi sebuah treenode p, bernilai tidak NULL jika berhasil */
+/* Mengirimkan treenode hasil alokasi sebuah elemen bernilai val
+   Jika alokasi berhasil, maka treenode tidak NULL, dan misalnya
+   menghasilkan p, maka p↑.info=val, p↑.child=NULL, p↑.nextchild=NULL
    Jika alokasi gagal, mengirimkan NULL */
 {
+    TreeNode *p = (TreeNode *)malloc(sizeof(TreeNode));
+    if (*p != NULL)
+    {
+        root(*p) = val;
+        child(*p) = NULL;
+        nextchild(*p) = NULL;
+    }
+    return *p;
+}
+
+boolean isTreeEmpty(TreeNode *p)
+/* Fungsi untuk mengecek apakah pohon kosong
+   Mengirimkan true jika p adalah pohon yang kosong */
+{
     /* KAMUS */
-    Address p;
 
     /* ALGORITMA */
-    p = (Address) malloc(sizeof(TreeNode));
-    if (p != NULL) {
-        ROOT(p) = val;
+    return (*p == NULL);
+}
+
+boolean isOneElmt(TreeNode *p)
+{
+    /* Fungsi untuk mengecek apakah pohon hanya terdiri dari satu elemen
+        Pohon tidak kosong dan tidak memiliki anak dan saudara berikutnya */
+    return (*p != NULL && child(*p) == NULL && nextchild(*p) == NULL);
+}
+
+void addChild(TreeNode *parent, TreeNode *child)
+/* I.S. p terdefinisi
+   F.S. child menjadi cabang atau daun baru dari parent
+/* proses: menambahkan anak ke sebuah node */
+{
+    if (child(parent) == NULL)
+    {
+        child(parent) = child;
+    }
+    else
+    {
+        TreeNode *sibling = child(parent);
+        while (nextchild(sibling) != NULL)
+        {
+            sibling = nextchild(sibling);
+        }
+        nextchild(sibling) = child;
     }
 }
 
-void deallocTreeNode (Address p)
-/* I.S. p terdefinisi 
-   F.S. p dikembalikan ke sistem 
-   Melakukan dealokasi/pengembalian address p */
+void deleteTree(TreeNode *tree, ElType val)
+/* I.S. p terdefinisi
+   F.S. p dikembalikan ke sistem
+   Melakukan dealokasi/pengembalian address p,
+   Menghapus tree akan dilakukan secara cascade,
+   dimana peranakannya ikut terhapus */
 {
-    /* KAMUS */
+    if (tree == NULL)
+    {
+        return;
+    }
 
-    /* ALGORITMA */
-    free(p);
-}
-
-boolean isTreeEmpty (BinTree p)
-/* Mengirimkan true jika p adalah pohon biner yang kosong */
-{
-    /* KAMUS */
-
-    /* ALGORITMA */
-    return (p == NULL);
-}
-
-boolean isOneElmt (BinTree p)
-/* Mengirimkan true jika p tidak kosong dan hanya terdiri atas 1 elemen */
-{
-    /* KAMUS */
-
-    /* ALGORITMA */
-    return (!isTreeEmpty(p) && LEFT(p) == NULL && RIGHT(p) == NULL);
-}
-
-boolean isUnerLeft (BinTree p)
-/* Mengirimkan true jika pohon biner tidak kosong, p adalah pohon unerleft: 
-   hanya mempunyai subpohon kiri */
-{
-    /* KAMUS */
-
-    /* ALGORITMA */
-    return (!isTreeEmpty(p) && LEFT(p) != NULL && RIGHT(p) == NULL);
-}
-
-boolean isUnerRight (BinTree p)
-/* Mengirimkan true jika pohon biner tidak kosong, p adalah pohon unerright: 
-   hanya mempunyai subpohon kanan */
-{
-    /* KAMUS */
-
-    /* ALGORITMA */
-    return (!isTreeEmpty(p) && LEFT(p) == NULL && RIGHT(p) != NULL);
-}
-
-boolean isBinary (BinTree p)
-/* Mengirimkan true jika pohon biner tidak kosong, p adalah pohon biner: 
-  mempunyai subpohon kiri dan subpohon kanan */
-{
-    /* KAMUS */
-
-    /* ALGORITMA */
-    return (!isTreeEmpty(p) && LEFT(p) != NULL && RIGHT(p) != NULL);
+    /* Hapus child dulu */
+    TreeNode *loc = child(tree);
+    TreeNode *prevChild = NULL;
+    while (loc != NULL)
+    {
+        TreeNode *next = nextchild(loc);
+        if (root(loc) == val)
+        {
+            loc = NULL;
+            // hapus nextchildnya
+            if (prevChild == NULL)
+            {
+                child(tree) = next;
+            }
+            else
+            {
+                nextchild(prevChild) = next;
+            }
+            free(loc);
+        }
+        else
+        {
+            deleteTree(loc, val);
+        }
+        prevChild = loc;
+        loc = next;
+    }
 }
 
 /* ****** Display Tree ***** */
-void printPreorder(BinTree p)
-/* I.S. p terdefinisi */
-/* F.S. Semua simpul p sudah dicetak secara preorder: akar, pohon kiri, dan pohon kanan. 
-   Setiap pohon ditandai dengan tanda kurung buka dan kurung tutup (). 
-   Pohon kosong ditandai dengan ().
-   Tidak ada tambahan karakter apa pun di depan, tengah, atau akhir. */
-/* Contoh: 
-   (A()()) adalah pohon dengan 1 elemen dengan akar A
-   (A(B()())(C()())) adalah pohon dengan akar A dan subpohon kiri (B()()) dan subpohon kanan (C()()) */
-{
-    /* KAMUS */
-
-    /* ALGORITMA */
-    printf("(");
-    if (!isTreeEmpty(p)) {
-        printf("%d", ROOT(p));
-        printPreorder(LEFT(p));
-        printPreorder(RIGHT(p));
-    }
-    printf(")");
-}
-
-void printInorder(BinTree p)
-/* I.S. p terdefinisi */
-/* F.S. Semua simpul p sudah dicetak secara inorder: pohon kiri, akar, dan pohon kanan. 
-   Setiap pohon ditandai dengan tanda kurung buka dan kurung tutup (). 
-   Pohon kosong ditandai dengan ().
-   Tidak ada tambahan karakter apa pun di depan, tengah, atau akhir. */
-/* Contoh: 
-   (()A()) adalah pohon dengan 1 elemen dengan akar A
-   ((()B())A(()C())) adalah pohon dengan akar A dan subpohon kiri (()B()) dan subpohon kanan (()C()) */
-{
-    /* KAMUS */
-
-    /* ALGORITMA */
-    printf("(");
-    if (!isTreeEmpty(p)) {
-        printInorder(LEFT(p));
-        printf("%d", ROOT(p));
-        printInorder(RIGHT(p));
-    }
-    printf(")");
-}
-
-void printPostorder(BinTree p)
-/* I.S. p terdefinisi */
-/* F.S. Semua simpul p sudah dicetak secara postorder: pohon kiri, pohon kanan, dan akar. 
-   Setiap pohon ditandai dengan tanda kurung buka dan kurung tutup (). 
-   Pohon kosong ditandai dengan ().
-   Tidak ada tambahan karakter apa pun di depan, tengah, atau akhir. */
-/* Contoh: 
-   (()()A) adalah pohon dengan 1 elemen dengan akar A
-   ((()()B)(()()C)A) adalah pohon dengan akar A dan subpohon kiri (()()B) dan subpohon kanan (()()C) */
-{
-    /* KAMUS */
-
-    /* ALGORITMA */
-    printf("(");
-    if (!isTreeEmpty(p)) {
-        printPostorder(LEFT(p));
-        printPostorder(RIGHT(p));
-        printf("%d", ROOT(p));
-    }
-    printf(")");
-}
-
-void printTreeLevel(BinTree p, int h, int l)
-{
-    /* KAMUS */
-    int N;
-
-    /* ALGORITMA */
-    if (!isTreeEmpty(p)) {
-        N = h * l;
-
-        while (N > 0) {
-            printf(" ");
-            N--;
-        }
-
-        printf("%d\n", ROOT(p));
-
-        printTreeLevel(LEFT(p), h, l + 1);
-        printTreeLevel(RIGHT(p), h, l + 1);
-    }
-}
-
-void printTree(BinTree p, int h)
+void printTree(TreeNode *node, int depth);
 /* I.S. p terdefinisi, h adalah jarak indentasi (spasi) */
 /* F.S. Semua simpul p sudah ditulis dengan indentasi (spasi) */
 /* Penulisan akar selalu pada baris baru (diakhiri newline) */
-/* Contoh, jika h = 2: 
-1) Pohon preorder: (A()()) akan ditulis sbb:
-A
-2) Pohon preorder: (A(B()())(C()())) akan ditulis sbb:
-A
-  B
-  C
-3) Pohon preorder: (A(B(D()())())(C()(E()()))) akan ditulis sbb:
+/* Contoh,
 A
   B
     D
   C
     E
-Note: Anda boleh membuat fungsi tambahan untuk membuat implementasi fungsi ini jika diperlukan
 */
 {
-    /* KAMUS */
+    if (node == NULL)
+    {
+        return;
+    }
 
-    /* ALGORITMA */
-    printTreeLevel(p, h, 0);
+    for (int i = 0; i < depth; i++)
+    {
+        printf("  ");
+    }
+    printf("%d\n", node->data);
+
+    printTree(node->child, depth + 1); // Cetak anak pertama
+    printTree(node->nextchild, depth); // Cetak saudara berikutnya
 }
+
+TreeNode *searchTree(TreeNode *node, int val)
+/* Fungsi untuk mencari val pada tree
+   Mengembalikan TreeNode ditemukannya val */
+{
+    if (node == NULL)
+    {
+        return NULL; // basis node tidak ditemukan
+    }
+
+    if (root(*node) == id)
+    {
+        return node; // basis node ditemukan
+    }
+
+    TreeNode *found = searchTree(child(*node), id);
+    if (found == NULL)
+    {
+        found = searchTree(nextchild(*node), id);
+    }
+
+    return found;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
